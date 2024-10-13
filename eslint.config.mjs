de1c8f886dbe98c6646e697b03414cf1d6dcdbb3
Bence Molnar <xjarekx@gmail.com>
2024-10-13 16:50:40 +0200

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import withNuxt from './.nuxt/eslint.config.mjs';
+import prettierConfig from 'eslint-config-prettier';
+
+export default withNuxt(
+  // your custom flat configs go here, for example:
+  // {
+  //   files: ['**/*.ts', '**/*.tsx'],
+  //   rules: {
+  //     'no-console': 'off' // allow console.log in TypeScript files
+  //   }
+  // },
+  // {
+  //   ...
+  // }
+  prettierConfig,
+  {
+    rules: {
+      'nuxt/prefer-import-meta': 'error',
+    },
+  },
+);
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  nitro: {
+    preset: 'static',
+  },
+  ssr: true,
+
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: ['@nuxt/image', '@nuxt/eslint'],
+  compatibilityDate: '2024-09-05'
+})
