813973c31e295646cc84589588c4a02a1c8ae58e
Bence Molnár <xjarekx@gmail.com>
2025-01-28 16:14:05 +0100

    Add new menu items

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="default.vue">
     <nav class="bg-gray-800 p-4">
-      <ul class="flex justify-center gap-x-4">
-        <li><a href="/" class="text-white">Köszöntő</a></li>
-        <li><a href="/eskuvo" class="text-white">Esküvő</a></li>
-        <li><a href="/lakodalom" class="text-white">Lakodalom</a></li>
+      <ul class="flex justify-center gap-x-4 text-white">
+        <li><a href="/">Köszöntő</a></li>
+        <li><a href="/eskuvo">Esküvő</a></li>
+        <li><a href="/lakodalom">Lakodalom</a></li>
+        <li><a href="/naszajandek">Nászajándék</a></li>
+        <li><a href="/visszajelzes">Visszajelzés</a></li>
       </ul>
     </nav>
     <slot />
---
+++ pages/naszajandek.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        Nászajándék
+    </div>
+    
+</template>
