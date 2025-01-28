2e8373a32a0ee64420ee6f025b53d3d9412ccc6c
Bence Molnár <xjarekx@gmail.com>
2025-01-28 19:33:39 +0100

    Remove revolut app link

--- components/RevolutLinkButton.vue
+++ components/RevolutLinkButton.vue
@@ -16,12 +16,12 @@ defineProps<{
       <RevolutIcon />
       {{ tag }}
     </a>
-    <a
+    <!-- <a
       :href="`revolut://send?tag=${tag}&currency=${currency}&amount=${amount}&note=${note}`"
       class="inline-flex sm:hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700"
     >
       <RevolutIcon />
       {{ tag }}
-    </a>
+    </a> -->
   </div>
 </template>
