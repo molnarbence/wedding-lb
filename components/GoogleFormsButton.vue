31e61eb0346230164a9abe8ffb144c1e80443851
Bence Molnár <xjarekx@gmail.com>
2025-01-28 20:01:52 +0100

    run format

--- components/GoogleFormsButton.vue
+++ components/GoogleFormsButton.vue
@@ -4,14 +4,13 @@ defineProps<{
 }>();
 </script>
 <template>
-    <div>
-      <a
-        :href="link"
-        class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center dark:bg-blue-600 dark:hover:bg-blue-700"
-      >
-        <FormsIcon />
-        Google Forms Link
-      </a>
-    </div>
-  </template>
-  
+  <div>
+    <a
+      :href="link"
+      class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center dark:bg-blue-600 dark:hover:bg-blue-700"
+    >
+      <FormsIcon />
+      Google Forms Link
+    </a>
+  </div>
+</template>
