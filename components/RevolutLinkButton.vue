8088419fd58fb37765530de40357476d3acb9d25
Bence Molnár <xjarekx@gmail.com>
2025-01-28 19:34:04 +0100

    remove hidden class

--- components/RevolutLinkButton.vue
+++ components/RevolutLinkButton.vue
@@ -11,7 +11,7 @@ defineProps<{
   <div>
     <a
       :href="`${encodeURI(`https://revolut.me/${tag}/${currency}${amount}/${note}`)}`"
-      class="hidden sm:inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700"
+      class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700"
     >
       <RevolutIcon />
       {{ tag }}
